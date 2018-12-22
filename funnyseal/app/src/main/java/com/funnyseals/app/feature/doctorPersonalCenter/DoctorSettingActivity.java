a05fe4f88e474cfee5e15e857b48ab8204e119e0
qianliang <2310404818+qianliang@users.noreply.github.com>
2018-12-22 18:16:56 +0800

    个人中心最新版4.0

--- funnyseal/app/src/main/java/com/funnyseals/app/feature/doctorPersonalCenter/DoctorSettingActivity.java
+++ funnyseal/app/src/main/java/com/funnyseals/app/feature/doctorPersonalCenter/DoctorSettingActivity.java
@@ -1,15 +1,24 @@
 package com.funnyseals.app.feature.doctorPersonalCenter;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.funnyseals.app.LoginActivity;
 import com.funnyseals.app.R;
 
 /**
  * 医生端
  * 设置界面 activity
  */
+@SuppressLint("Registered")
 public class DoctorSettingActivity extends AppCompatActivity {
+    private TextView tv_doctor_setting_finish;
+    private ImageButton ib_doctor_setting_return;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -19,6 +28,28 @@ public class DoctorSettingActivity extends AppCompatActivity {
     }
 
     public void init () {
+        tv_doctor_setting_finish=findViewById(R.id.tv_doctor_setting_finish);
+        ib_doctor_setting_return=findViewById(R.id.ib_doctor_setting_return);
+        tv_doctor_setting_finish.setOnClickListener(new addListeners());
+        ib_doctor_setting_return.setOnClickListener(new addListeners());
 
+    }
+    /**
+     * 监听事件
+     */
+    private class addListeners implements View.OnClickListener {
+        @Override
+        public void onClick (View v) {
+            switch (v.getId()) {
+                case R.id.ib_doctor_setting_return:
+                    finish();
+                    break;
+                case R.id.tv_doctor_setting_finish:
+                    startActivity(new Intent(DoctorSettingActivity.this, LoginActivity.class));
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
